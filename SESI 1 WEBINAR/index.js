c61f4d4c7d12fff2bfdcdeeb50eec38a88aba76f
Rizal <129894811+RizalMahdi@users.noreply.github.com>
2024-06-04 01:17:13 +0700

    Add files via upload

---
+++ SESI 1 WEBINAR/index.js
@@ -0,0 +1,25 @@
+/*
+Algoritma:
+1. Buat variable r 
+2. buat variable phi
+3. buatlah variable volume bola
+4. isi variable VolumeBola dengan rumus = 3/4 * Phi * r * r * r
+5. Cetak VolumeBola
+*/
+
+/* 
+Pseudocode :
+1. isi variable phi dengan 3.14
+2. isi variable r dengan ANGKA
+3. isi VolumeBola dengan 3/4 * phi * r * r * r
+4. Cetak VolumeBola
+*/
+
+const r = 7
+alert ('isi r adalah 7')
+const phi = 3.14
+alert ('isi phi adalah 3.14')
+const VolumeBola = 3/4 * phi * 7 * 7 * 7
+alert ('Rumus Volume bola adalah = 3/4 * phi * r * r * r')
+console.log(VolumeBola)
+alert ('Maka jawaban volume bola adalah ' + VolumeBola)
